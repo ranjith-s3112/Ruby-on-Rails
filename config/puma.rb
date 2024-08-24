5c43445caabe8f676c20651f4406016d9150b4d1
ranjith-s3112 <ranjithmax1111@gmail.com>
2024-08-24 09:43:38 +0530

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,10 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 4500 }
+
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 4500)}"
+
 
 # Specifies the `environment` that Puma will run in.
 #
