adffd20cad280307dd663d241290e4a9aaa88acb
ranjith-s3112 <ranjithmax1111@gmail.com>
2024-08-25 20:39:28 +0530

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   after_create :delete_post
 
   def delete_post
-    DeletePostJob.set(wait: 10.seconds).perform_later(self.id)
+    DeletePostJob.set(wait: 20.seconds).perform_later(self.id)
   end
 end
